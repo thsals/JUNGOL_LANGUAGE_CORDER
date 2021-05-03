93ce5519e1ef7c95d33f0a06ecd38ec0331ffa64
손민 <80296020+thsals@users.noreply.github.com>
2021-05-03 21:51:30 +0900

    Create 5.cpp
    http://www.jungol.co.kr/bbs/board.php?bo_table=pbank&wr_id=24&sca=1050

---
+++ 선택제어문/5.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+    #include <stdio.h>
+    using namespace std;
+ 
+    int main()
+    {
+        int a;
+        scanf("%d ", &a);
+        if (a == 1 || a == 3 || a == 5 || a == 7 || a == 8 || a == 10 || a == 12) {
+            printf("31");
+        }
+        else if (a == 2) {
+            printf("28");
+        }
+        else if (a == 4 || a == 6 || a==9 || a == 11) {
+            printf("30");
+        }
+ 
+        return 0;
+    }
