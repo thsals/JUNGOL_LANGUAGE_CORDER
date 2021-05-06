163cf14564467104286fea2e63c8e71284fbd75a
손민 <80296020+thsals@users.noreply.github.com>
2021-05-06 11:11:57 +0900

    Create 1.cpp
    http://www.jungol.co.kr/bbs/board.php?bo_table=pbank&wr_id=30&sca=1070

---
+++ 반복제어문2/1.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+using namespace std;
+ 
+int main()
+{
+    int n;
+    int a;
+    scanf("%d",&n);
+    for (a = 0;a < n;a++) {
+        printf("JUNGOL\n");
+    }
+     
+    return 0;
+}
