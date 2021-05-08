80bfcb428b7da679bf876a51f4f17acccc0b3ecb
손민 <80296020+thsals@users.noreply.github.com>
2021-05-08 20:11:12 +0900

    Create 1.cpp
    http://www.jungol.co.kr/bbs/board.php?bo_table=pbank&wr_id=50&sca=1090

---
+++ 배열/1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+ 
+int main()
+{
+    char a,b,c,d,e,f,g,h,i,j;
+    scanf("%c %c %c %c %c %c %c %c %c %c",&a,&b,&c,&d,&e,&f,&g,&h,&i,&j);
+    printf("%c %c %c %c %c %c %c %c %c %c",j,i,h,g,f,e,d,c,b,a);
+    return 0;
+}
