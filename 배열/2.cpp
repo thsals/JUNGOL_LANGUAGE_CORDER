337f638b1a2ef715f0f612ea4df199552b21e825
손민 <80296020+thsals@users.noreply.github.com>
2021-05-08 20:18:55 +0900

    Create 2.cpp
    http://www.jungol.co.kr/bbs/board.php?bo_table=pbank&wr_id=51&sca=1090

---
+++ 배열/2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+ 
+int main()
+{
+    int a,b,c,d,e;
+    scanf("%d %d %d %d %d",&a,&b,&c,&d,&e);
+    printf("%d",a+c+e);
+    return 0;
+}
