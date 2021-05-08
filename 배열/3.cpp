4b245cb2595450a8209fb6cc3c907b0262e7d796
손민 <80296020+thsals@users.noreply.github.com>
2021-05-08 20:19:24 +0900

    Create 3.cpp
    http://www.jungol.co.kr/bbs/board.php?bo_table=pbank&wr_id=52&sca=1090

---
+++ 배열/3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+ 
+int main()
+{
+    int a,b,c,d,e,f,g,h,i,j;
+    scanf("%d %d %d %d %d %d %d %d %d %d",&a,&b,&c,&d,&e,&f,&g,&h,&i,&j);
+    printf("odd : %d\neven : %d",a+c+e+g+i,b+d+f+h+j);
+    return 0;
+}
